a3c84da8b3d04624699b5a418115e09c0a617e1b
Markus Rathgeb <maggu2810@gmail.com>
2018-06-13 21:49:52 +0200

    fix imports
    Signed-off-by: Markus Rathgeb <maggu2810@gmail.com>

--- src/main/java/de/edgelord/stdf/reading/FileReader.java
+++ src/main/java/de/edgelord/stdf/reading/FileReader.java
@@ -2,7 +2,9 @@ package de.edgelord.stdf.reading;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Scanner;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.stream.Collectors;
 
 public class FileReader {
 
@@ -16,11 +18,11 @@ public class FileReader {
     * Returns the whole content of the file given in the constructor, by using a Scanner
     * and appending each line to a StringBuilder. 
     *
-    * @return      the content of the file
+    * @return the content of the file
+    * @throws java.io.IOException on an I/O error
     */
     
     public String readFile() throws IOException {
-
         return Files.lines(file.toPath(), StandardCharsets.UTF_8).collect(Collectors.joining(System.lineSeparator()));
     }
     
